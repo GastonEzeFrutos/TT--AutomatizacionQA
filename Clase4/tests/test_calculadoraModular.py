2ad192ac3a8065fceb98ff7613beb566a668a3b2
GastiFrutos <gfrutos97@gmail.com>
2026-09-17 23:15:33 -0300

    pytest prueba 2

--- Clase4/tests/test_calculadoraModular.py
+++ Clase4/tests/test_calculadoraModular.py
@@ -1,31 +1,31 @@
 import pytest
 
-from Clase4.test_calculadora_modular import suma,resta,multiplicacion,division
+from calculadoraModular import sumar,restar,multiplicar,dividir
 
 @pytest.mark.operador
 def test_suma():
-    resultado = suma(8,3)
+    resultado = sumar(5,3)
 
     assert resultado == 8
 
 @pytest.mark.operador
 def test_resta():
-    resultado = resta(10,4)
+    resultado = restar(10,4)
 
     assert resultado == 6
 
 @pytest.mark.operador
 def test_multiplicacion():
-    resultado = multiplicacion(5,3)
+    resultado = multiplicar(5,3)
 
     assert resultado == 15
 
 @pytest.mark.operador
 def test_division():
-    resultado = division("10",2)
+    resultado = dividir(10,2)
 
     assert resultado == 5
 
 def test_dividir_por_cero():
     with pytest.raises(ZeroDivisionError):
-        division(50,0)
+        dividir(50,0)
